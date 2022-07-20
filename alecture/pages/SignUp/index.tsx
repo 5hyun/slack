294534aeda43d4080519d8a7685a7c637a8a3ab6
5hyun <qhslsl@gmail.com>
2022-07-20 16:49:45 +0900

    swr까지 들음

---
+++ alecture/pages/SignUp/index.tsx
@@ -0,0 +1,119 @@
+import useInput from '@hooks/useInput';
+import React, { useCallback, useState } from 'react';
+import axios from 'axios';
+import { Form, Label, Input, LinkContainer, Button, Header, Error, Success } from './styles';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+  const [email, onChangeEmail, setEmail] = useInput('');
+  const [nickname, onChangeNickname, setNickname] = useInput('');
+  const [password, , setPassword] = useInput('');
+  const [passwordCheck, , setPasswordCheck] = useInput('');
+  //  비밀번호 일치 확인
+  const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+
+  const onChangePassword = useCallback(
+    (e: any) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e: any) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      if (!mismatchError && nickname) {
+        console.log('서버로 회원가입하기');
+
+        // 요청하기 전에 한번 초기화하는게 좋다.
+        setSignUpError('');
+        setSignUpSuccess(false);
+
+        axios
+          .post('/api/users', {
+            email,
+            nickname,
+            password,
+          })
+          // 성공하는 경우
+          .then((response) => {
+            console.log(response);
+            setSignUpSuccess(true);
+          })
+          // 실패하는 경우
+          .catch((error) => {
+            console.log(error.response);
+            setSignUpError(error.response.data);
+          })
+          // 성공을 하든 실패를 하든 무조건 실행되는 코드
+          .finally(() => {});
+      }
+      // // 혹은 try catch 문으로도 사용 가능하다.
+      // try {} catch(err) {
+
+      // } finally {
+
+      // }
+    },
+    [email, nickname, password, passwordCheck, mismatchError],
+  );
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp ;<Link to="/login">로그인 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
+};
+
+export default SignUp;

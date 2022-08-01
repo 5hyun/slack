2bf5545851d74610a7176a0a95a89158d6095a49
5hyun <qhslsl@gmail.com>
2022-08-01 17:27:00 +0900

    SWR Devtools 적용

--- alecture/client.tsx
+++ alecture/client.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
-
 import App from '@layouts/App';
+import SWRDevtools from '@jjordy/swr-devtools';
 
 render(
   <BrowserRouter>
-    <App />
+    {/*  이렇게 추가하면 배포하면 Devtool가 안보인다.*/}
+    {process.env.NODE_ENV === 'production' ? (
+      <App />
+    ) : (
+      <SWRDevtools>
+        <App />
+      </SWRDevtools>
+    )}
   </BrowserRouter>,
   document.querySelector('#app'),
 );
